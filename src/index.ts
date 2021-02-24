a51c2cef0dba24b8182bd7ac006d4cb56d84ee3b
jprochazk <honza.spacir@gmail.com>
2021-02-24 10:51:26 +0100

    patch text encoder/decoder + add lockfile

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+// @ts-ignore
+if (process?.versions?.node != null) {
+    // ensure TextEncoder and TextDecoder are available in global scope in node
+    // this probably doesn't work in Electron using build tools which patch
+    // global.require()
+    // @ts-ignore 
+    if (globalThis.TextEncoder == null) globalThis.TextEncoder = globalThis.require("util").TextEncoder
+    // @ts-ignore
+    if (globalThis.TextDecoder == null) globalThis.TextDecoder = globalThis.require("util").TextDecoder
+}
 
 export { Reader } from "./reader"
 export { Writer } from "./writer"
